5844b427c95756236a3977ae635e25626345c41a
Daniel <5037816657cg@gmail.com>
2023-05-05 13:59:44 -0700

    enable retrieving table data

--- frontend/src/Pages/LogIn/LogIn.js
+++ frontend/src/Pages/LogIn/LogIn.js
@@ -1,7 +1,22 @@
+const getResult = () => {
+  fetch(`http://localhost:5000`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("success");
+      console.log(result);
+    })
+    .catch((error) => console.log(error));
+};
+
 const LogIn = () => {
   return (
     <div>
-      <div>LogIn</div>
+      <button onClick={getResult}>Get Current Table</button>
     </div>
   );
 };
