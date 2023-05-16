4ef9c87bc9e4072db64ae2f38d106fb554e50720
Daniel <5037816657cg@gmail.com>
2023-05-16 15:56:22 -0700

    Sign In page

--- frontend/src/Pages/LogIn/LogIn.js
+++ frontend/src/Pages/LogIn/LogIn.js
@@ -1,21 +1,77 @@
-const getResult = () => {
-  fetch(`http://127.0.0.1:5000/`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("success");
-      console.log(result);
-    })
-    .catch((error) => console.log(error));
-};
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const getResult = async (e) => {
+    e.preventDefault();
+    await fetch(`http://127.0.0.1:5000/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("success");
+        console.log(result);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const validateUser = async (e) => {
+    e.preventDefault();
+    let base = "http://127.0.0.1:5000/validate";
+    base += `?username=${userInfo.username}&password=${userInfo.password}`;
+
+    const response = await fetch(base, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+
+    if (!response.ok) {
+      alert(result.error);
+    } else {
+      console.log(result);
+      navigate("/manage");
+    }
+  };
+
   return (
     <div>
+      <h1>TripleC HR Management System</h1>
+      <form>
+        <label>Username:</label>
+        <br />
+        <input
+          type="text"
+          onChange={(e) => {
+            setUserInfo({ ...userInfo, username: e.target.value });
+          }}
+          value={userInfo.username}
+        />
+        <br />
+        <label>Password:</label>
+        <br />
+        <input
+          type="password"
+          onChange={(e) => {
+            setUserInfo({ ...userInfo, password: e.target.value });
+          }}
+          value={userInfo.password}
+        />
+      </form>
+      <button className="submit" onClick={validateUser}>
+        Sign In
+      </button>
       <button onClick={getResult}>Get Current Table</button>
     </div>
   );
