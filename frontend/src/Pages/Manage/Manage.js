f14fd1fe34179c1ddc79f4833122428d6b23088b
evelyn037 <xih037@ucsd.edu>
2023-05-29 14:29:01 -0700

    a

--- frontend/src/Pages/Manage/Manage.js
+++ frontend/src/Pages/Manage/Manage.js
@@ -1,4 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from "react"
 const Manage = () => {
+  const [id, setId] = useState("")
+  const [fullname, setFullname] = useState("");
+  const [allmembers, setAllmembers] = useState([]);
+  useEffect(() => {
+    const FetchMember = async (e) => {
+      e.preventDefault();
+      let base = "http://127.0.0.1:5000/member";
+      const response = await fetch (base, {
+        method:"GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      if (!response.ok) {
+        alert(result.error);
+      } else {
+        setAllmembers(result)  
+      }
+    }; 
+  }),[];
   return (
     <div>
       <h1>Manage</h1>
