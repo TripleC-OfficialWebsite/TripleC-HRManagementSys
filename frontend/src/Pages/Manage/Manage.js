1a3041cf9b6cef6cbf28cb9c6802bdc6b8d1b7ed
Daniel <5037816657cg@gmail.com>
2023-05-28 22:54:19 -0700

    simple changes

---
+++ frontend/src/Pages/Manage/Manage.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const Manage = () => {
+  const [memberIds, setMemberIds] = useState([]);
+  const [memberNames, setMemberNames] = useState([]);
+
+  const fetchMembers = async () => {
+    const response = await fetch(
+      `http://127.0.0.1:5000/member_range?page=0&limit=10`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    console.log("123");
+    const ids = data.map((member) => member._id);
+    const names = data.map((member) => member.fullname);
+
+    setMemberIds(ids);
+    setMemberNames(names);
+  };
+
+  useEffect(() => {
+    fetchMembers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Manage</h1>
+      {memberIds.map((id, index) => (
+        <div key={id}>
+          <p>ID: {id}</p>
+          <p>Name: {memberNames[index]}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Manage;
